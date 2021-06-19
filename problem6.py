a07c1cfc7845e34b99f37435a03d1585506e08d4
Victor Amonde <amondev00@gmail.com>
2021-06-19 22:37:35 +0300

    Added problem6.py

---
+++ problem6.py
@@ -0,0 +1,30 @@
+import os
+import sys
+import random
+import string
+
+
+def main():
+    files = [f"file_{''.join(random.choices(string.ascii_lowercase, k=6))}.pdf" for _ in range(100)]
+    # files that have been completely processed
+    processed = dict(
+        zip(
+            random.choices(files, k=10),
+            random.choices(range(1, 100), k=10)
+        )
+    )
+    # files currently processing
+    processing = random.choices(list(set(files).difference(processed.keys())), k=15)
+    # files yet to be processed
+    # unprocessed = None
+    print(f"unprocessed files: {processing}")
+    table = processed.items()
+    for keys, values in table:
+        if values >= 50:
+            print(keys, values)
+
+    return os.EX_OK
+
+
+if __name__ == "__main__":
+    sys.exit(main())
