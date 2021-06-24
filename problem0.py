6554af57171df3ff689e8800f4d9fe53d682a067
Victor Amonde <amondev00@gmail.com>
2021-06-24 19:39:02 +0300

    Modify problem0 and added problem1

--- problem0.py
+++ problem0.py
@@ -5,13 +5,18 @@ import sys
 def main():
     dt = dict()
     word = input("Enter 10 character word: ")
-    for i in word:
-        if i not in dt:
-            dt[i] = 1
+    for i in range(len(word)):
+        if len(word) == 10:
+            for i in word:
+                if i not in dt:
+                    dt[i] = 1
+                else:
+                    dt[i] = dt[i] + 1
+            print(f"dt: {dt}")
+            break
         else:
-            dt[i] = dt[i] + 1
-    print(f"dt: {dt}")
-    return os.EX_OK
+            print("Word should have 10 characters")
+            return os.EX_OK
 
 
 if __name__ == "__main__":
